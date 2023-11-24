e02de37b92f79481e3381c9c32407be5f995fb15
Rochan Avlur Venkat <rochan.avlur@gmail.com>
2023-11-24 22:40:13 +0530

    Added error handling, TODO: mem leak

--- src/region.cpp
+++ src/region.cpp
@@ -19,23 +19,27 @@ using namespace pybind11::literals;
 
 namespace py = pybind11;
 
-/* Box-type for upper-left and lower-right coordinates */
-typedef pair<pair<int, int>, pair<int, int>> box_t;
-
-/* Function ported from Python to convert continuous coordinates of action to
-  upper-left and lower-right coordinates given the image size and view size */
-box_t continuous_to_coords(pair<float, float> action, pair<int, int> img_sz, pair<int, int> view_sz)
+/**
+ * @brief Converts continuous coordinates of action to upper-left coordinates
+ * given the image size and view size.
+ *
+ * @param action The continuous coordinates to convert.
+ * @param img_sz The size of the image.
+ * @param view_sz The size of the view.
+ * @return VipsRect containing upper-left and rectangle height and width.
+ */
+VipsRect continuous_to_coords(pair<float, float> action, pair<int, int> img_sz, pair<int, int> view_sz)
 {
-  float x = action.first;
-  float y = action.second;
+    // Normalize coordinates to the range [0, 1]
+    float x = (action.first + 1) / 2;
+    float y = (action.second + 1) / 2;
 
-  x = (x + 1) / 2;
-  y = (y + 1) / 2;
+    // Calculate upper-left coordinates
+    int up_left_x = static_cast<int>((img_sz.first - view_sz.first) * x);
+    int up_left_y = static_cast<int>((img_sz.second - view_sz.second) * y);
+    pair<int, int> up_left{};
 
-  pair<int, int> up_left{int((img_sz.first - view_sz.first) * x), int((img_sz.second - view_sz.second) * y)};
-  pair<int, int> lower_right{up_left.first + view_sz.first, up_left.second + view_sz.second};
-
-  return {up_left, lower_right};
+    return VipsRect{up_left_x, up_left_y, view_sz.first, view_sz.second};
 }
 
 /* BaseEnv class */
@@ -43,125 +47,224 @@ class BaseEnv
 {
 
 public:
-  vector<string> files;
-  vector<int> classes;
-  pair<int, int> view_sz;
-  int max_episode_len = 100;
-  int timestep = 0;
-  int dataset_index = 0;
+    vector<string> files;
+    vector<int> classes;
+    pair<int, int> view_sz;
+    int max_episode_len = 100;
+    int timestep = 0;
+    int dataset_index = 0;
+    py::array_t<int> a;
 
-  /* Hack to initialize */
-  VImage image = VImage::new_from_memory((void *)NULL, (size_t)1, 1, 1, 1, VIPS_FORMAT_UCHAR);
+    /* Hack to initialize */
+    VImage image = VImage::new_from_memory((void *)NULL, (size_t)1, 1, 1, 1, VIPS_FORMAT_UCHAR);
 
-  /* Initialize files that we need to read */
-  BaseEnv(py::dict dataset, py::tuple view_sz, int max_episode_len)
-  {
-    for (auto &item : dataset) {
-        /* dict keys are file paths, values are class_idx */
-        const std::string &key = item.first.cast<std::string>();
-        const int &value = item.second.cast<int>();
+    /**
+     * @brief Constructor initalizing environment.
+     *
+     * @param py::dict dataset with keys (path to image) and values (classes).
+     * @param py::tuple view size
+     * @param int maximum episode length
+     */
+    BaseEnv(py::dict dataset, py::tuple view_sz, int max_episode_len)
+    {
 
-        BaseEnv::files.push_back(key);
-        BaseEnv::classes.push_back(value);
-    }
-
-    BaseEnv::view_sz = view_sz.cast<pair<int, int>>();
-    BaseEnv::max_episode_len = max_episode_len;
-  }
-
-  void _init_random_image()
-  {
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_int_distribution<> dis(0, BaseEnv::files.size()-1);
-
-    BaseEnv::dataset_index = dis(gen);
-
-    /* pick random image from file list */
-    BaseEnv::image = VImage::new_from_file(&BaseEnv::files[BaseEnv::dataset_index][0], VImage::option()->set("access", VIPS_ACCESS_RANDOM));
-  }
-
-  /* Initalizes 3D py::array_t of type with pixels of the region in memory */
-  py::array_t<int> get_region(box_t patch)
-  {
-    /* (view_sz.first, view_sz.second, 3) */
-    const size_t _h = (size_t)BaseEnv::view_sz.first;
-    const size_t _w = (size_t)BaseEnv::view_sz.second;
-    const size_t _c = (size_t)3;
-
-    constexpr size_t elsize = sizeof(int);
-    size_t shape[3]{_c, _h, _w};
-    size_t strides[3]{_w * _h * elsize, _w * elsize, elsize};
-    auto a = py::array_t<int>(shape, strides);
-    auto view = a.mutable_unchecked<3>();
-
-    VipsRect r{patch.first.first, patch.first.second, BaseEnv::view_sz.first, BaseEnv::view_sz.second};
-
-    VipsRegion *region;
-    if(!(region = vips_region_new(BaseEnv::image.get_image())))
-        vips_error_exit(NULL);
-
-    if (vips_region_prepare(region, &r))
-        vips_error("vips_region_prepare error!", NULL);
-
-    for (int y = 0; y < r.height; y++) {
-        VipsPel *p = VIPS_REGION_ADDR(region, r.left, r.top + y);
-        for (int x = 0; x < r.width; x++){
-            for (int b = 0; b < BaseEnv::image.bands(); b++){
-                view(b, y, x) = *p++;
+        try
+        {
+            if (dataset.size() == 0 || view_sz.size() != 2 || max_episode_len <= 0)
+            {
+                throw invalid_argument("Invalid arguments. 'dataset' must not be empty, 'view_sz' must be a tuple of size 2, and 'max_episode_len' must be greater than 0.");
             }
+
+            for (auto &item : dataset)
+            {
+                /* dict keys are file paths, values are class_idx */
+                const string &key = item.first.cast<string>();
+                const int &value = item.second.cast<int>();
+
+                files.push_back(key);
+                classes.push_back(value);
+            }
+
+            this->view_sz = view_sz.cast<pair<int, int>>();
+            this->max_episode_len = max_episode_len;
+
+
+            const size_t _h = static_cast<size_t>(this->view_sz.first);
+            const size_t _w = static_cast<size_t>(this->view_sz.second);
+            const size_t _c = static_cast<size_t>(3);
+
+            constexpr size_t elsize = sizeof(int);
+            size_t shape[3]{_c, _h, _w};
+            size_t strides[3]{_w * _h * elsize, _w * elsize, elsize};
+            a = py::array_t<int>(shape, strides);
+        }
+        catch (const exception &e)
+        {
+            // Catch any C++ exceptions and convert them to Python exceptions
+            throw py::value_error(e.what());
         }
     }
-    return a;
-  }
 
-  /* reset the environment: select a random image and location to read */
-  py::tuple reset()
-  {
-    _init_random_image();
+    /**
+     * @brief Initialize the image for the episode.
+     */
+    void _init_random_image()
+    {
+        try
+        {
+            if (files.empty())
+            {
+                throw runtime_error("No files available for initialization");
+            }
 
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_real_distribution<> dis(0.0f, 1.0f);
+            random_device rd;
+            mt19937 gen(rd());
+            uniform_int_distribution<> dis(0, files.size() - 1);
 
-    /* random (x, y) coordinates */
-    pair<float, float> points{float(dis(gen)), float(dis(gen))};
-    box_t patch = continuous_to_coords(points, {BaseEnv::image.width(), BaseEnv::image.height()}, BaseEnv::view_sz);
+            dataset_index = dis(gen);
 
-    BaseEnv::timestep = 0;
+            /* pick random image from file list */
+            image = VImage::new_from_file(&files[dataset_index][0], VImage::option()->set("access", VIPS_ACCESS_RANDOM));
 
-    py::dict info("timestep"_a=timestep, "target"_a=BaseEnv::classes[BaseEnv::dataset_index]);
-    /* return (obs, info) */
-    return py::make_tuple(get_region(patch), info);
-  }
+            if (image.width() <= 0 || image.height() <= 0)
+            {
+                throw runtime_error("Failed to load image. Ensure that the image file is valid and accessible.");
+            }
+        }
+        catch (const exception &e)
+        {
+            // Catch any C++ exceptions and convert them to Python exceptions
+            throw py::value_error(e.what());
+        }
+    }
 
-  /* step the environment: select new observation and return */
-  py::tuple step(py::tuple action)
-  {
-    float action_x = action.attr("__getitem__")(0).attr("__float__")().cast<float>();
-    float action_y = action.attr("__getitem__")(1).attr("__float__")().cast<float>();
+    /**
+     * @brief Given a rectangular region within the image, prepare an array with
+     * pixel values from the region.
+     *
+     * @param VipsRect* Pointer to a VipRect struct specifying the rectangular
+     *      region to prepare as array_t.
+     * @return py::array_t<int> containing pixels of the rectangular region.
+     */
+    py::array_t<int> get_region(VipsRect *patch)
+    {
+        try
+        {
+            auto view = a.mutable_unchecked<3>();
 
-    /* Get box based on action */
-    box_t patch = continuous_to_coords({action_x, action_y}, {BaseEnv::image.width(), BaseEnv::image.height()}, BaseEnv::view_sz);
+            VipsRegion *region;
+            if (!(region = vips_region_new(image.get_image())))
+                throw runtime_error("Failed to create VipsRegion");
 
-    BaseEnv::timestep += 1;
+            if (vips_region_prepare(region, patch))
+            {
+                throw runtime_error("Failed to prepare VipsRegion. Ensure that the region coordinates are within the image boundaries.");
+            }
 
-    py::dict info("timestep"_a=timestep, "target"_a=BaseEnv::classes[BaseEnv::dataset_index]);
-    /* return (next_obs, reward, done, truncated, info) */
-    return py::make_tuple(get_region(patch), 0, (timestep >= BaseEnv::max_episode_len ? py::bool_(true) : py::bool_(false)), py::bool_(false), info);
-  }
+            for (int y = 0; y < patch->height; y++)
+            {
+                VipsPel *p = VIPS_REGION_ADDR(region, patch->left, patch->top + y);
+                for (int x = 0; x < patch->width; x++)
+                {
+                    for (int b = 0; b < image.bands(); b++)
+                    {
+                        view(b, y, x) = *p++;
+                    }
+                }
+            }
+            return a;
+        }
+        catch (const exception &e)
+        {
+            // Catch any C++ exceptions and convert them to Python exceptions
+            throw py::value_error(e.what());
+        }
+    }
+
+    /**
+     * @brief Reset the environment.
+     *
+     * @return py::tuple containing observation (py::array_t<int>) and info
+     *      (py::dict).
+     */
+    py::tuple reset()
+    {
+        _init_random_image();
+
+        random_device rd;
+        mt19937 gen(rd());
+        uniform_real_distribution<> dis(0.0f, 1.0f);
+
+        /* random (x, y) coordinates */
+        pair<float, float> points{static_cast<float>(dis(gen)), static_cast<float>(dis(gen))};
+        VipsRect patch = continuous_to_coords(points, {image.width(), image.height()}, view_sz);
+
+        timestep = 0;
+
+        py::dict info("timestep"_a = timestep, "target"_a = classes[dataset_index]);
+        /* return (obs, info) */
+        return py::make_tuple(get_region(&patch), info);
+    }
+
+    /**
+     * @brief Step in the environment.
+     *
+     * @param py::tuple Action tuple.
+     * @return py::tuple Contains
+     *      next observation (py::array_t<int>), reward (int),
+     *      done (py::bool_(false)), truncated (py::bool), info (py::dict)
+     */
+    py::tuple step(py::tuple action)
+    {
+        try
+        {
+            if (action.size() != 2)
+            {
+                throw invalid_argument("Action must be a tuple of size 2 and values must be in (0, 1)");
+            }
+            float action_x = action[0].cast<float>();
+            float action_y = action[1].cast<float>();
+
+            /* Get box based on action */
+            VipsRect patch = continuous_to_coords({action_x, action_y}, {image.width(), image.height()}, view_sz);
+
+            timestep += 1;
+
+            py::dict info("timestep"_a = timestep, "target"_a = classes[dataset_index]);
+            /* return (next_obs, reward, done, truncated, info) */
+            return py::make_tuple(get_region(&patch), 0, py::bool_(false), (timestep >= max_episode_len ? py::bool_(true) : py::bool_(false)), info);
+        }
+        catch (const exception &e)
+        {
+            // Catch any C++ exceptions and convert them to Python exceptions
+            throw py::value_error(e.what());
+        }
+    }
+
+    /**
+     * @brief Close the environment.
+     *
+     * @return py::none.
+     */
+    py::none close()
+    {
+        // TODO: memory cleanup!
+
+        return py::none();
+    }
 };
 
 PYBIND11_MODULE(vipsenv, m)
 {
-  py::class_<BaseEnv>(m, "BaseEnv")
-      .def(py::init<py::dict, py::tuple, int>(), py::arg("dataset"), py::arg("view_sz"), py::arg("max_episode_len"))
-      .def("reset", &BaseEnv::reset, "reset method of environment")
-      .def("step", &BaseEnv::step, "step method of environment", py::arg("action"))
-      .def_readwrite("files", &BaseEnv::files)
-      .def_readwrite("classes", &BaseEnv::classes)
-      .def_readwrite("view_sz", &BaseEnv::view_sz)
-      .def_readwrite("max_episode_len", &BaseEnv::max_episode_len)
-      .def_readonly("timestep", &BaseEnv::timestep)
-      .def_readwrite("dataset_index", &BaseEnv::dataset_index);
+    py::class_<BaseEnv>(m, "BaseEnv")
+        .def(py::init<py::dict, py::tuple, int>(), py::arg("dataset"), py::arg("view_sz"), py::arg("max_episode_len"))
+        .def("reset", &BaseEnv::reset, "Reset environment.")
+        .def("step", &BaseEnv::step, "Step in environment.", py::arg("action"))
+        .def("close", &BaseEnv::close, "Close environment.")
+        .def_readwrite("files", &BaseEnv::files)
+        .def_readwrite("classes", &BaseEnv::classes)
+        .def_readwrite("view_sz", &BaseEnv::view_sz)
+        .def_readwrite("max_episode_len", &BaseEnv::max_episode_len)
+        .def_readonly("timestep", &BaseEnv::timestep)
+        .def_readwrite("dataset_index", &BaseEnv::dataset_index);
 }
