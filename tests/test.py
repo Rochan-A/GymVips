52077b46bdb87a3676d40d1d7d2f64af2daf9faa
Rochan Avlur <rochan.avlur@gmail.com>
2023-12-15 15:07:58 -0600

    Added test

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,30 @@
-import vipsenv as m
+import random
+from tqdm import tqdm
+import vipsenv
 
-assert m.__version__ == "0.0.1"
-# TODO
+if __name__=='__main__':
+    # initialize vips
+    vipsenv.init(__file__)
+
+    dataset = {"./img.jpg": 1}
+    num_envs = 2
+    view_sz = (256, 256)
+    max_episode_len = 100
+
+    envs = vipsenv.AsyncVipsEnv(num_envs, dataset, view_sz, max_episode_len)
+
+    for i in tqdm(range(100)):
+        obs, infos = envs.reset()
+        assert obs.shape == (num_envs, 3, *view_sz), f"Obs size did not match! Expected {(num_envs, 3, *view_sz)}, got {obs.shape}!"
+
+        while True:
+            obs, reward, dones, truncateds, infos = envs.step(((random.random(), random.random()), (random.random(), random.random())))
+            assert obs.shape == (num_envs, 3, *view_sz), f"Obs size did not match! Expected {(num_envs, 3, *view_sz)}, got {obs.shape}!"
+
+            if dones.any() == True:
+                break
+
+    envs.close()
+    del envs
+
+    vipsenv.shutdown()
